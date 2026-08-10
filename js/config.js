ad98f6c5883cbf4c259456fa4344bbbc8c34cbc9
nchapananga-beep <nchapananga@unisced.edu.mz>
2026-08-10 12:16:12 +0200

    Configura endpoint do Apps Script para JURISLAB Tribunal

---
+++ js/config.js
@@ -0,0 +1,5 @@
+window.JURISLAB_CONFIG = Object.freeze({
+  API_URL: "https://script.google.com/macros/s/AKfycbyFzl8x8Kazn2ek0j5N8qF0f5beYNOSrNSfxx837FEF0do_gF3lzW3Z1UCvo9eeTROB/exec",
+  NOME: "JURISLAB UnISCED",
+  MODULO_TRIBUNAL: "JURISLAB Tribunal"
+});
